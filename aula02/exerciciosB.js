f249efb3ecd1f32a3cfd41feb7939277726734b4
Pablo <104365552+pablosGomes@users.noreply.github.com>
2026-03-02 08:12:11 -0300

    Add files via upload

---
+++ aula02/exerciciosB.js
@@ -0,0 +1,13 @@
+let a = 30;
+let b = 20;
+let c = a + b;
+
+console.log(c);
+
+b = 10;
+
+console.log(b , c);
+
+c = a + b;
+
+console.log(a, b, c);
---
+++ aula02/exerciciosD.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = a + 1;
+
+a = b + 1;
+b = a + 1;
+
+console.log(a);
+
+a = b + 1;
+
+console.log(a , b);
