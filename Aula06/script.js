1758c1faea3ab6fad32c5b205fd6763bfe261159
Pablo <104365552+pablosGomes@users.noreply.github.com>
2026-03-30 10:48:32 -0300

    Add files via upload

---
+++ Aula06/script.js
@@ -0,0 +1,10 @@
+// a)Crie uma função que receba dois numero e retorne o maior dele 
+
+function num (a, b){
+    return Math.max (a, b);
+}
+
+let resultado = num(30, 200);
+console.log ("O maior numero é ",  resultado)
+
+
